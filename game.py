e8e6af209c161e274623b62ee26a0797572c51f5
tnair3 <nairstejas@gmail.com>
2024-09-20 23:26:08 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,43 +1,78 @@
 import pygame
 import constants
+import wallgeneration
+
 pygame.init()
 
-
+screen = constants.screen
 pygame.display.set_caption("NEA: Pacman Game")
 
 #OBJECTS ============================================================================================================== OBJECTS
 class Player():
-    def __init__(self, speed, posX=constants.SCREEN_WIDTH/2, posY=constants.SCREEN_HEIGHT/2, size=16, colour=constants.COLOURS[constants.YELLOW]):
+    def __init__(self, speed, posX=constants.CENTREX, posY=constants.CENTREY + 100, size=20, colour=constants.COLOURS[constants.YELLOW]):
         self.colour = colour
         self.size = size
-        self.posx = posX
-        self.rightborder = self.posx + size
-        self.posy = posY
-        self.bottomborder = self.posy + size
+        self.posx = posX - self.size/2
+        self.posy = posY - self.size/2
         self.speed = speed
-        self.character = pygame.Rect(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2, self.size, self.size)
+        self.character = pygame.Rect(self.posx, self.posy, self.size, self.size)
 
     def movement(self, key):
-        for k in range(0, self.speed):
-            if key[pygame.K_a]:
-                self.character.move_ip(-1, 0)
-                self.posx -= 1
-                self.rightborder -= 1
+        if key == 0:
+            return 0
+        
+        else:
+            nocollisions = 0
+            for i in range(0, len(wallgeneration.walls[0])):
+                if not self.character.colliderect(wallgeneration.walls[0][i].wall):
+                    nocollisions += 1
 
-            elif key[pygame.K_d]:
-                self.character.move_ip(1, 0)
-                self.posx += 1
-                self.rightborder += 1
+            if nocollisions == 4:
+                for k in range(0, self.speed):
+                    if key == "a":
+                        self.character.move_ip(-1, 0)
+                        self.posx -= 1
 
-            elif key[pygame.K_w]:
-                self.character.move_ip(0, -1)
-                self.posy -= 1
-                self.bottomborder -= 1
+                    elif key == "d":
+                        self.character.move_ip(1, 0)
+                        self.posx += 1
 
-            elif key[pygame.K_s]:
-                self.character.move_ip(0, 1)
-                self.posy += 1
-                self.bottomborder += 1
+                    elif key == "w":
+                        self.character.move_ip(0, -1)
+                        self.posy -= 1
+
+                    elif key == "s":
+                        self.character.move_ip(0, 1)
+                        self.posy += 1 
+                return key
+            
+            else:
+                for k in range(0, self.speed):
+                    if key == "a":
+                        self.character.move_ip(1, 0)
+                        self.posx += 1
+
+                    elif key == "d":
+                        self.character.move_ip(-1, 0)
+                        self.posx -= 1
+
+                    elif key == "w":
+                        self.character.move_ip(0, 1)
+                        self.posy += 1
+
+                    elif key == "s":
+                        self.character.move_ip(0, -1)
+                        self.posy -= 1
+                return 0
+
+    def detectghostcollision(self):
+        if (self.character.colliderect(ghosts[0].entity) or 
+            self.character.colliderect(ghosts[1].entity) or 
+            self.character.colliderect(ghosts[2].entity) or 
+            self.character.colliderect(ghosts[3].entity)):
+            return True
+        else:
+            return False
 
     def getposx(self):
         return self.posx
@@ -49,12 +84,12 @@ class Player():
         return self.speed
 
 class Ghost():
-    def __init__(self, colourchosen, speed, offset, posX=constants.SCREEN_WIDTH/2, posY=(constants.SCREEN_HEIGHT/2) + 100, size=16, colour=constants.COLOURS):
+    def __init__(self, colourchosen, speed, offset, posX=constants.CENTREX, posY=(constants.CENTREY) - 100, size=20, colour=constants.COLOURS):
         self.colour = colour[colourchosen]
         self.colourindex = colourchosen
         self.size = size
-        self.posx = posX + offset
-        self.posy = posY
+        self.posx = posX + offset - (self.size/2)
+        self.posy = posY - self.size/2
         self.speed = speed
         self.entity = pygame.Rect(self.posx, self.posy, self.size, self.size)
 
@@ -89,10 +124,14 @@ def displayEntities():
     pygame.draw.rect(constants.screen, constants.COLOURS[ghosts[3].getcolour()], ghosts[3].entity)
 
 def ghostsFollow():
-    ghosts[0].follow(user.getposx(), user.getposy())
-    ghosts[1].follow(user.getposx(), user.getposy())
-    ghosts[2].follow(user.getposx(), user.getposy())
-    ghosts[3].follow(user.getposx(), user.getposy())
+    for i in range(0, 4):
+        ghosts[i].follow(user.getposx(), user.getposy())
+
+def checkforcollisions(objectselfvar):
+    pass
+
+def displaygameover():
+    pass
 
 #DEFINING VARIABLES =================================================================================================== DEFINING VARIABLES
 user = Player(2)
@@ -105,23 +144,42 @@ ghosts = [Ghost(constants.RED, 1, 500),
 clock = pygame.time.Clock()
 
 run = True
-import wallgeneration
+continueloop = True
+gameover = False
+centreofscreen = pygame.Rect(constants.CENTREX, constants.CENTREY, 1, 1)
+mostrecentkey = "0"
 
 #GAME LOOP ============================================================================================================ GAME LOOP
-while run:
+while continueloop:
     clock.tick(120)
-    constants.screen.fill(constants.COLOURS[5])
+    constants.screen.fill(constants.COLOURS[constants.BLACK])
+    if run:
+        displayEntities()
+        wallgeneration.drawborderwalls()
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            mostrecentkey = "w"
+        elif key[pygame.K_a]:
+            mostrecentkey = "a"
+        elif key[pygame.K_s]:
+            mostrecentkey = "s"
+        elif key[pygame.K_d]:
+            mostrecentkey = "d"
+        mostrecentkey = user.movement(mostrecentkey)
+        ghostsFollow()
 
-    displayEntities()
-    wallgeneration.drawborderwalls()
+        pygame.draw.rect(constants.screen, constants.COLOURS[constants.WHITE], centreofscreen)
 
-    user.movement(pygame.key.get_pressed())
-    ghostsFollow()
+        gameover = user.detectghostcollision()
+        if gameover:
+            run = False
+    else:
+        wallgeneration.drawborderwalls()
+        displaygameover()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-
+            continueloop = False
     pygame.display.update()
 
 pygame.quit()
