32f8bb426d04f84dc261e4ddb534479cf2e0a5db
tnair3 <nairstejas@gmail.com>
2024-09-22 20:04:21 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
 import pygame
+import math
 pygame.init()
 
-SCREEN_WIDTH = 1220
-SCREEN_HEIGHT = 680
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 950
 
 CENTREX = SCREEN_WIDTH / 2
 CENTREY = SCREEN_HEIGHT / 2
@@ -14,7 +15,10 @@ COLOURS = [(255, 0, 0),         #RED
            (255, 255, 0),       #YELLOW
            (0, 0, 0),           #BLACK
            (255, 255, 255),     #WHITE
-           (5, 21, 156)          #BLUE
+           (5, 21, 156),        #BLUE
+           (62, 62, 62),        #GREY
+           (0, 255, 31),        #GREEN
+           (110, 0, 0)          #DARKRED
            ] 
 RED = 0
 CYAN = 1
@@ -24,3 +28,8 @@ YELLOW = 4
 BLACK = 5
 WHITE = 6
 BLUE = 7
+GREY = 8
+GREEN = 9
+DARKRED = 10
+
+PI = math.pi
